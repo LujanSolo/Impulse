1f6a336dd4a29bee9b1d9bbd2f505c3083b2fd4a
LujanSolo <lujansolo@proton.me>
2022-10-01 21:07:49 -0700

    Built out js/profile.js. Added notes to controllers/index.js. Added more functionality to homeRoutes.js. changed "loginRoutes" to "userRoutes" for clarity

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
+//* /api routes for CRUD operations and GET json objects
 router.use('/api', apiRoutes);
+
+//* / for all handlebars (HTML)
 router.use('/', homeRoutes);
 
 module.exports = router;
