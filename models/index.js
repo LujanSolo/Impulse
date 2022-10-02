ff261c895ec4a2a312a31a9b67acf65f76ebf492
LujanSolo <lujansolo@proton.me>
2022-10-01 21:26:03 -0700

    added notes to models/index.js. Changed filename of AcquiredGoods.js to AcquiredGood.js to match with model naming conventions

--- models/index.js
+++ models/index.js
@@ -1,14 +1,26 @@
 const User = require('./User');
-const AcquiredGoods = require('./AcquiredGoods');
-// const LifeEvents = require('./LifeEvents');
+
+//* I changed it to ('./AcquiredGood') here to keep with model naming conventions (singular). Same thing with ('./LifeEvent) below - JL
+const AcquiredGoods = require('./AcquiredGood');
+// const LifeEvents = require('./LifeEvent');
 
 
 User.hasMany(AcquiredGoods, {
   foreignKey: 'user_id',
 });
 
+//*  I think we need to add this Association  -JL
+// AcquiredGoods.belongsTo(User, {
+//   foreignKey: 'user_id',
+// });
+
 User.hasMany(LifeEvents, {
   foreignKey: 'user_id',
 });
+
+//* uncertain if we need to add a LifeEvents.belongsTo(User) here. if so, i added it and commented it out.-JL
+// LifeEvents.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
 
 module.exports = { User, LifeEvents, AcquiredGoods };
