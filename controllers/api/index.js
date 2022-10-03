03df4d715e5f6121c1d5c9e944fe2882614a2d5a
LujanSolo <lujansolo@proton.me>
2022-10-03 11:26:35 -0700

    1. updated server.js to include session info for authorization. 2. Added POST routes to life event and goods routes. 3. Corrected GET routes in lifeEvents and Goods Routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,7 @@ const acquiredGoodsRoutes = require('./acquiredGoodsRoutes');
 const lifeEventRoutes = require('./lifeEventRoutes');
 const userRoutes = require('./userRoutes');
 
-router.use('/acquiredGoods', acquiredGoodsRoutes);
+router.use('/acquiredGoodsRoutes', acquiredGoodsRoutes);
 router.use('/lifeEventRoutes', lifeEventRoutes);
 router.use('/userRoutes', userRoutes);
 
