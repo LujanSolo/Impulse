66ce5e189f20c508e6a9b6f7e4ac37845a6551d2
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-10-03 14:15:38 -0700

    fixed log in

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,5 +1,5 @@
 const logout = async () => {
-  const response = await fetch('/api/users/logout', {
+  const response = await fetch('/api/userRoutes', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   });
