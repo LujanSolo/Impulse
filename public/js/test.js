51fd78369266b207e7cb8b4b4ba8de53c18e6d91
LujanSolo <lujansolo@proton.me>
2022-10-04 13:51:56 -0700

    added cloudinary

--- public/js/test.js
+++ public/js/test.js
@@ -327,3 +327,18 @@ freeSpiritStartbtn.on("click", startGame);
 workLifeStartbtn.on("click", startGame);
 
 playAgainbtn.on("click", playAgain);
+
+ 
+var myWidget = cloudinary.createUploadWidget({
+  cloudName: 'dcfdy30wq', 
+  uploadPreset: 'my_preset'}, (error, result) => { 
+    if (!error && result && result.event === "success") { 
+      console.log('Done! Here is the image info: ', result.info); 
+    }
+  }
+)
+
+document.getElementById("upload_widget").addEventListener("click", function(){
+    myWidget.open();
+  }, false);
+
